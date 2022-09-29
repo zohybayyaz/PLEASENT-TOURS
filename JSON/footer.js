dcff54de13e24c1fddab44cf53bcf9c4afb73dc8
zohybayyaz <zohybakashonu@gmail.com>
2022-09-29 09:50:46 -0700

    PLEASENT TOURS

---
+++ JSON/footer.js
@@ -0,0 +1,62 @@
+document.getElementById("footer").innerHTML +=`
+<section class="foot">
+    <div class="b">
+      <h3 class="p-3"><b><i> <u> Usefull Links</u></i></b></h3>
+      <ul class="foot-list">
+        <li><a href="#" class="nav-link text-white" style="text-align: justify ;"><i> Customer Care</i></a></li>
+        <li><a href="#" style="text-align: justify ;" class="nav-link text-white"><i> Contact Us</i></a></li>
+        <li><a href="#" style="text-align: justify ;" class="nav-link text-white"><i> Help & Support</i></a></li>
+      </ul>
+    </div>
+    <div class="b">
+      <h3 class="p-3"><b><i> <u> Locate Us</u></i></b></h3>
+      <p>Address : </p>
+      <p> <i class="fa-brands fa-whatsapp" style="font-size:20px;"></i> <a href="#" class="text-white" style="text-decoration: none;"> +923172585014 </a></p>
+    </div>
+    <div class="b">
+      <h3 class="p-3 text-center"><b><i> <u> Gallery</u></i></b></h3>
+      <div class="d-flex w-100 ">
+      <div class="">
+        <a href="">
+          <img src="Images/friends.jpg" alt="img.png" width="200px" class="p-1">
+        </a>
+        </div>
+        <div class="">
+          <a href="">
+            <img src="Images/friends 2.jpg" alt="img.png" width="200px" class="p-1">
+          </a>
+        </div>
+      </div>
+      <div class="d-flex w-100">
+        <div class="card-img-top">
+          <a href="">
+            <img src="Images/desert.jpg" alt="img.png" width="200px" class="p-1"  >
+          </a>
+        </div>
+        <div class="card-img-top">
+          <a href="">
+            <img src="Images/lake.jpg" alt="img.png" width="200px" class="p-1">
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="b ">
+      <h3 class="p-3 text-center"><b><i> <u> Join Us </u></i></b></h3>
+      <div class="d-flex" style="margin-left: 60px;">
+        <a href="" class="nav-link"><i class="fa-brands fa-facebook" style="font-size: 40px; color: blue;"></i></a>     
+        <a href="" class="nav-link">
+          <i class="fa-brands fa-instagram" style="color: #fb3958; 
+          font-size: 40px;" ></i>
+        </a>     
+        <a href="" class="nav-link">
+          <i class="fa-brands fa-twitter" style="color: rgb(4, 156, 250);font-size: 40px; "></i>
+        </a>     
+      </div>
+      <div class="" style="margin-left:80px ; padding-top: 20px;">
+        <h5>Submit your Review.</h5>
+        <input type="text" placeholder="Enter the Review." style="border-radius: 20px; border: 2px solid black; padding:10px;">
+      </div>
+    </div>
+    
+  </section>
+`
---
+++ JSON/header.js
@@ -0,0 +1,40 @@
+
+document.getElementById("header").innerHTML +=`
+
+<nav class="navbar">
+  <div class="logo">
+
+  </div>
+  <div class="nav">
+    <ul class="nav-navbar">
+      <li class="nav-item">
+        <a href="" class="nav-link  text-info">Home</a>
+      </li>
+      <li class="nav-item">
+        <a href="" class="nav-link text-info">About Us</a>
+      </li>
+      <li class="nav-item dropdown text-info">
+        <a class="nav-link dropdown-toggle text-info" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Categories
+        </a>
+        <ul class="dropdown-menu dropdown-menu-info" aria-labelledby="navbarDarkDropdownMenuLink">
+          <li><a class="dropdown-item" href="#">Family Tours</a></li>
+          <li><a class="dropdown-item" href="#">Couple Tours</a></li>
+          <li><a class="dropdown-item" href="#">Friends Tours</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a href="" class="nav-link text-info">Contact Us<a>
+
+        </li>
+      <li class="nav-item">
+        <a href="" class="nav-link text-info">FAQ</a>
+      </li>
+
+    </ul>
+    
+  </div>
+ 
+
+</nav>
+`
